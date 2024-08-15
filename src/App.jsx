9feb5e84c88693e4609bbd1d36c79d008a3270a7
Yana Kozma <jane.kozma@gmail.com>
2024-08-15 15:23:40 -0400

    imprt axios and fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import './App.css'
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 function App() {
+const [newsList, setNewsList] = useState([])
+  useEffect(() => {
+    const fetchDataFromApi = async() => {
+      const response = await axios.get("https://api.spaceflightnewsapi.net/v4/articles/");
+      setNewsList(response.data.results);
+    }
+    fetchDataFromApi()
+  }, [])
+
   return (
-    <div>
+    <div className="App">
       <div className="title">
         <h1>Space News</h1>
       </div>
       <div className="newsContainer">
-
-
+      {newsList.map((val, idx) => {
+          return <div key={idx}>{val.title}</div>
+        })}
       </div>
     </div>
   )
 }
 
-export default App;
+export default App
